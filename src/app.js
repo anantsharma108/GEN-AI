6a6afa7b3fadc79ecff7adccd44824f704f84581
Anant Sharma <anantwwwww@gmail.com>
2026-07-31 23:20:24 +0530

    create controller for generating interview report

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 const express= require('express');
 const app=express();
 const authRoutes=require('./routes/auth.routes');
+const interviewRouter = require("./routes/interview.routes");
 const cookieParser=require('cookie-parser')
 
 app.use(express.json());
 app.use(cookieParser());
 
-/*using routes*/
-app.use('/api/auth',authRoutes);
+
+
+/* using all the routes here */
+app.use("/api/auth", authRouter)
+app.use("/api/interview", interviewRouter)
 
 module.exports=app;
